646be1323849cbb16a14a66e4301199a2440322d
Jon Soriano Sanjuan <158002831+jonathansoriano@users.noreply.github.com>
2026-06-29 14:29:10 -0500

    Added Routing and added NavLinks to navigate our pages in our app

---
+++ react-router-demo/src/AppLayout.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { NavLink, Outlet } from 'react-router-dom';
+
+//When renaming file name from App.jsx to AppLayout.jsx
+//You need to change the name of the component
+function AppLayout() {
+
+  const [view, setView] = useState("home");
+
+  return (
+    <>
+      <div>
+        <nav>
+          {/* Replaced out Buttons with NavLink tags to navigate our Pages*/}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+
+        <div>
+          {/* Whatever the router renders, it's going to be displayed here*/}
+          <Outlet/>
+        </div>
+
+      </div>
+    </>
+  );
+
+}
+//And change the name you're exporting here...
+export default AppLayout
--- react-router-demo/src/main.jsx
+++ react-router-demo/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from  'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppLayout from './AppLayout.jsx'
+import { Home } from './pages/Home.jsx';
+import { About } from './pages/About.jsx';
+
+//I needed to change the name of "App.jsx" to "AppLayout.jsx"
+//I also needed to change the name I used to import that component from
+//import App from './App.jsx' to import AppLayout from './AppLayout.jsx'. 
+//Otherwise I would get a Uncaught ReferenceError: AppLayout is not defined
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children: [
+      {path: `/`, element: <Home/>},
+      {path: `/about`, element: <About/>},
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router}/>
+);
