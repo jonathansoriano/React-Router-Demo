19165da0d478b89ecf0c9f265258467ead850e28
Jon Soriano Sanjuan <158002831+jonathansoriano@users.noreply.github.com>
2026-06-29 15:21:14 -0500

    Updated file names, also explained what Declaritive Navigation is with comments

--- react-router-demo/src/AppLayout.jsx
+++ react-router-demo/src/AppLayout.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './AppLayout.css'
 import { NavLink, Outlet } from 'react-router-dom';
 
 //When renaming file name from App.jsx to AppLayout.jsx
@@ -10,6 +10,7 @@ function AppLayout() {
       <div>
         <nav>
           {/* Replaced out Buttons with NavLink tags to navigate our Pages*/}
+          {/* NavLink and Link are Declaritive way to creating our routes in our App*/}
           <NavLink to="/">Home</NavLink>
           {` | `}
           <NavLink to="/about">About</NavLink>
